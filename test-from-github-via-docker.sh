0e6ed805b73714d88c8f23bc00b8391c86bbfc86
CompulsiveCoder <compulsivecoder@gmail.com>
2016-07-04 12:21:28 +1000

    Disabled potentially obsolete script console output line

--- test-from-github-via-docker.sh
+++ test-from-github-via-docker.sh
@@ -1,5 +1,4 @@
 BRANCH=$1
-TEST_CATEGORY=$2
 
 if [ -z "$BRANCH" ]; then
     BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
@@ -9,11 +8,5 @@ if [ -z "$BRANCH" ]; then
     BRANCH="master"
 fi
 
-if [ -z "$TEST_CATEGORY" ]; then
-    TEST_CATEGORY="Unit"
-fi
-
 echo "Branch: $BRANCH"
-#echo "Tests: $TEST_CATEGORY"  # TODO: Reimplement or remove support for specifying test type
-
 docker run -it compulsivecoder/ubuntu-mono /bin/bash -c "curl https://raw.githubusercontent.com/CompulsiveCoder/TileSim/$BRANCH/test-from-github.sh | sh -s $BRANCH"
